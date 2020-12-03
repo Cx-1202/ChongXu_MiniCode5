7f8b724daaa5ad4fcdd07f01194d9d4827ff56db
Cx-1202 <chongxu2468@gmail.com>
2020-12-03 15:06:08 +0800

    Initial commit

---
+++ CodeMiniTest5_ChongXu_source/Assets/Script/GMController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GMController : MonoBehaviour
+{
+    int timeleft;
+    public static GMController gmInstance;
+
+    public GameObject PlayerGO;
+    public GameObject addEnergyPrefab;
+    public GameObject minusEnergyPrefab;
+    public Text TimerText;
+
+    public int numberofSpawn;
+    public float levelTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( gmInstance == null)
+        {
+            gmInstance = this;
+        }
+
+        //instance addEnergyPrefab At Random Position
+            for (int i = 0; i < numberofSpawn; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0.4f, Random.Range(-15, 15));
+
+                if (Random.Range(0, 2) < 1)
+                {
+                    Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+                }
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( levelTime > 0)
+        {
+            levelTime -= Time.deltaTime;
+            timeleft = (int)levelTime;
+            TimerText.text = ("Timer : ") + timeleft.ToString();
+        }
+        else
+        {
+            levelTime = 0;
+            TimerText.text = ("Times up ! ");
+            SceneManager.LoadScene("EndScene");
+        }
+        
+    }
+
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
+    
+     public void AddTime()
+     {
+        levelTime += 5;
+            for (int i = 0; i < 1; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0.4f, Random.Range(-15, 15));
+
+                if (Random.Range(0, 2) < 1)
+                {
+                    Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+                }
+            }
+     }
+
+
+}
---
+++ CodeMiniTest5_ChongXu_source/Assets/Script/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    int EnergyCount = 0;
+    int speed = 10;
+    public Animator PlayerAnimation;
+    public GameObject PlayerGO;
+    public GameObject EnergyCube;
+    public Text Energy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Energy.text = ("Energy : " + EnergyCount);
+
+        if (GMController.gmInstance.levelTime > 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                PlayerAnimation.SetBool("Move", true);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 270, 0);
+                PlayerAnimation.SetBool("Move", true);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                PlayerAnimation.SetBool("Move", true);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                PlayerAnimation.SetBool("Move", true);
+            }
+            else
+            {
+                PlayerAnimation.SetBool("Move", false);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            PlayerAnimation.SetBool("Move", false);
+        }
+
+        if ( EnergyCount >= 30)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+        else if (EnergyCount < 0)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.gameObject.CompareTag("Energy"))
+        {
+            EnergyCount += 5;
+            Destroy(other.gameObject);
+            GMController.gmInstance.AddTime();
+        }
+        else if (other.gameObject.CompareTag("Energys"))
+        {
+            EnergyCount -= 10;
+            Destroy(other.gameObject);
+            GMController.gmInstance.AddTime();
+        }
+    }
+}
